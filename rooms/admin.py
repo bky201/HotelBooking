560de4e84f177d22c8f656fadb4b225255438f14
bek <bekrye@gmail.com>
2023-07-23 00:03:22 +0200

    Add admin panel search field, lsit display and filter

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Room)
 class RoomAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'available_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'available_on')
     summernote_fields = ('content')
 
 # @admin.register(Room)
