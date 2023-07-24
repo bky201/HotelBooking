96d4ee9fa78c0cdb7795424e5aee1bd3b84c3a05
bek <bekrye@gmail.com>
2023-07-24 20:49:26 +0200

    Add content to home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from roombooking.models import Room
 
 
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'home/index.html'
+    model = Room
+    context_object_name = 'roomlist'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:4]
