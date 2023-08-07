d9071d72e3b0e0566edaaef39a58c54662baefd4
bek <bekrye@gmail.com>
2023-08-07 22:31:24 +0200

    Add booking room detail

--- booking/settings.py
+++ booking/settings.py
@@ -28,7 +28,7 @@ DEVELOPMENT = os.environ.get('DEVELOPMENT', False)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['roombooking-9c7bc437dd80.herokuapp.com', 'localhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['*']
