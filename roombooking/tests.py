57f81c60071ff43940263486cd79fd9148bb53f2
bek <bekrye@gmail.com>
2023-08-12 17:50:51 +0200

    Create Set up functionality

--- roombooking/tests.py
+++ roombooking/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Room, Booking
 
-# Create your tests here.
+class TestViews(TestCase):
+    """
+    Test cases for booking app as logged in user
+    """
+    def setUp(self):
+        """ Setup test """
+        username = "bookingads"
+        password = "54321add"
+        user_model = get_user_model()
+        #Create user
+        self.user = user_model.objects.create_user(
+            username=username,
+            password=password,
+            is_superuser=True,
+            is_staff=True
+        )
+        logged_in = self.client.login(username=username, password=password)
+        self.assertTrue(logged_in)
